739caab0e32c5f10521b9b0b5eab6fc48f79ed36
Andrew Caniglia <andrewcaniglia@gmail.com>
2018-04-07 21:57:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ function add (a, b) {
 return a + b;
 }
 
-function subtract () {
-  a -= b;
+function subtract (a, b) {
+return a - b;
 }
 
 function multiply () {
-  a * b;
+return a * b;
 }
+
+function 
